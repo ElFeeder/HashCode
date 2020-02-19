5250a654435e222dc9db9b6070764ff4efc54cfe
Diogo Abreu <diogofariaabreu@protonmail.com>
2020-02-19 21:47:04 +0000

    Code with error of 0.001% or less

---
+++ Practice/Practice_Tree.h
@@ -0,0 +1,18 @@
+#ifndef PRACTICE_TREE_H
+#define PRACTICE_TREE_H
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+
+#define TOLE 0.82
+#define MULTI 0.0135
+
+
+/*  Main.c  */
+void writeOnFile(int *final, int differentPizza, int *slices, char *originalName);
+unsigned long RecursiveSearch (int *final, int *slices,float tolerance,unsigned long people,int startPoint, unsigned long lastResult);
+FILE* openFile(FILE *fp, char* name, char *mode);
+#endif
